182740e725923770454e7bb59f243104f8886ff9
ioexcept <ioexcept@yahoo.com>
2025-01-24 13:13:49 -0600

    working on the db script

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,17 +2,15 @@ SET GLOBAL time_zone = 'America/Chicago';
 
 USE ceresdb;
 
--- Create users table
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(64) NOT NULL,
     email VARCHAR(64) UNIQUE NOT NULL,
-    department VARCHAR(64) NOT NULL
+    department VARCHAR64255) NOT NULL
 );
 
--- Create user_reports table
 CREATE TABLE user_reports (
-    user_id INT NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
     seen_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
